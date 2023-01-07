fc7e2e260597004bd0b49173867b737574da9f67
Michael Chirozidi <maikl.tigriv@gmail.com>
2023-01-07 18:56:16 +0100

    built structure of project

---
+++ src/kotlin/rest/src/main/kotlin/michigang1/me/rest/RestApplication.kt
@@ -0,0 +1,11 @@
+package michigang1.me.rest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestApplication>(*args)
+}
